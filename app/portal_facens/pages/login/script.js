4dab1844b80208327a8e77e6c55201fe17874164
Diego Juan Isaquiel <diego.mizael@facens.br>
2022-11-21 16:12:33 -0300

    criando perfil do diego

--- app/portal_facens/pages/login/script.js
+++ app/portal_facens/pages/login/script.js
@@ -7,6 +7,10 @@ function validateForm() {
     window.location.href = '../main/home/home.html';
     return false;
   }
+  else if((username == "222545@facens.br") && (password == "diego_isaquiel")){
+    window.location.href = '../main/second-student-page/second-student-page.html';
+    return false;
+  }
   else {
     alert("Não foi possível fazer Login. Cheque as credenciais e tente novamente!");
   }
